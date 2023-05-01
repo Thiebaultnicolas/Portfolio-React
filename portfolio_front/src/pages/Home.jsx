4458c1739f2f87894110de19f8192fc715c64c9f
Thiebault Nicolas <nicolasthiebault80870@yahoo.com>
2023-05-02 01:16:51 +0200

    ajout projet pokdédex

--- portfolio_front/src/pages/Home.jsx
+++ portfolio_front/src/pages/Home.jsx
@@ -9,6 +9,7 @@ import imageohmyfood from '../assets/screen_Ohmyfood.PNG'
 import imagekanap from '../assets/screen_Kanap.PNG'
 import imagepanthere from '../assets/screen_Panthere.PNG'
 import imagekasa from '../assets/screen_Kasa.PNG'
+import imagePokédex from '../assets/screen_Pokédex.png'
 import '../styles/Home.css'
 
 
@@ -31,12 +32,13 @@ const Home = () => {
                         <h1>Portfolio </h1>
                         <div className="projecthome">
                             <Project image={imagekasa}title="Kasa" description="Création d'une application web de location immobilière" code="Html5-Css3-JS-React" website="https://thiebault-nicolas-projet7-kasa.netlify.app/"  sourcecode="https://github.com/Thiebaultnicolas/P7-Kasa" />
-                            <Project image={imageohmyfood} title="Oh my food" description="Intégration d'une maquette avec animation" code="Html5-Css3" website="https://thiebaultnicolas.github.io/Projet3-Ohmyfood/"  sourcecode="https://github.com/Thiebaultnicolas/Projet3-Ohmyfood"/>
-                            <Project image={imagekanap} title="Kanap" description="Construction d'un site e-commerce" code="Html5-Css3-JS" website="http://thiebault-nicolas.fr:3001/html"  sourcecode="https://github.com/Thiebaultnicolas/projet5-Construisez-un-site-e-commerce-en-JavaScript" />
+                            <Project image={imageohmyfood} title="Oh my food" description="Intégration d'une maquette avec animation" code="Html5-Css3" website="https://thiebaultnicolas.github.io/Ohmyfood/"  sourcecode="https://github.com/Thiebaultnicolas/Projet3-Ohmyfood"/>
+                            <Project image={imagePokédex} title="Kanap" description="Construction d'un Pokédex" code="Html5-Css3-JS-Angular" website="https://ng-pokemon-app-f8efc.web.app/login"  sourcecode="https://github.com/Thiebaultnicolas/Pokemon-pokedex-Angular" />
                         </div>
                         <div className="projecthome2">
-                            <Project image={imagepanthere}title="La Panthère" description="Optimisation d'un site web existant" code="Référencement-SEO" website="https://thiebaultnicolas.github.io/Projet4/"  sourcecode="https://github.com/Thiebaultnicolas/Projet4" />
-                            <Project image={imagebooki} title="Booki" description="Intégration d'une maquette" code="Html5-Css3" website="https://thiebaultnicolas.github.io/Projet2-Booki/"  sourcecode="https://github.com/Thiebaultnicolas/Projet2-Booki" />
+                            <Project image={imagepanthere}title="La Panthère" description="Optimisation d'un site web existant" code="Référencement-SEO" website="https://thiebaultnicolas.github.io/debuggez-votre-site-SEO/"  sourcecode="https://github.com/Thiebaultnicolas/Projet4" />
+                            <Project image={imagebooki} title="Booki" description="Intégration d'une maquette" code="Html5-Css3" website="https://thiebaultnicolas.github.io/Booki/"  sourcecode="https://github.com/Thiebaultnicolas/Projet2-Booki" />
+                            <Project image={imagekanap} title="Kanap" description="Construction d'un site e-commerce" code="Html5-Css3-JS" website="http://thiebault-nicolas.fr:3001/html"  sourcecode="https://github.com/Thiebaultnicolas/projet5-Construisez-un-site-e-commerce-en-JavaScript" />
                         </div>
                     </div>
                 </div> 
